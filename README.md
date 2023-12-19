637850844797225c99d74303e5d620bf96b09d2a
vladislav918 <103207883+vladislav918@users.noreply.github.com>
2023-12-19 19:25:55 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# pro-prof.com sources
+
+Исходный код класса, реализующего максимально просто целочисленную длинную арифметику.
+Описание методов можно найти тут: https://pro-prof.com/forums/topic/long-integer-arithmetic-cplusplus
+
+Проект содержит модульные тесты (реализованы с помощью Qt test framework).
---
+++ src/form.cpp
@@ -0,0 +1,24 @@
+#include "form.h"
+#include "ui_form.h"
+
+Form::Form(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Form)
+{
+    ui->setupUi(this);
+
+    connect(ui->pushButton, SIGNAL(clicked()),
+            this, SLOT(start()));
+}
+
+Form::~Form()
+{
+    delete ui;
+}
+
+void Form::start() {
+    QString str = ui->str->text();
+    QString sub_str = ui->sub_str->text();
+
+    ui->result->setText("result.....");
+}
